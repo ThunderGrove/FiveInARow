5cdb6346d0653f1b478054e443f81ca706aa5227
ThunderGrove <rene699f@edu.easj.dk>
2020-04-27 09:36:59 +0200

    Moved UI into a package and added lib for SQLite.

--- src/LinuxMain.java
+++
@@ -1,6 +0,0 @@
-public class LinuxMain{
-    //This class is needed under Linux to run JavaFX when JavaFX libs are placed in Java lib folder.
-    public static void main(String[] args){
-        Main.main(args);
-    }
-}
---
+++ src/ui/LinuxMain.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class LinuxMain{
+    //This class is needed under Linux to run JavaFX when JavaFX libs are placed in Java lib folder.
+    public static void main(String[] args){
+        Main.main(args);
+    }
+}
